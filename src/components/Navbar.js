53148d1ea39b885264ea21111308d7daafbbb8ba
Michael Chen <michaelchengaming@gmail.com>
2020-06-04 11:39:56 -0700

    add navbar with temporary inline styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,60 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>placeholder for navbar</h1>
-    </div>
+    <nav
+      style={{
+        // temporary before we decide on css
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: "70px",
+        backgroundColor: "pink",
+      }}
+    >
+      <span
+        style={{
+          // temporary before we decide on css
+          marginLeft: "40px",
+        }}
+      >
+        IMAGE PLACEHOLDER
+      </span>
+      <ul
+        style={{
+          // temporary before we decide on css
+          listStyle: "none",
+          display: "flex",
+          flexDirection: "row",
+          marginRight: "40px",
+          justifyContent: "flex-end",
+        }}
+      >
+        <li
+          style={{
+            // temporary before we decide on css
+            marginLeft: "40px",
+          }}
+        >
+          Link 1
+        </li>
+        <li
+          style={{
+            // temporary before we decide on css
+            marginLeft: "40px",
+          }}
+        >
+          Link 2
+        </li>
+        <li
+          style={{
+            // temporary before we decide on css
+            marginLeft: "40px",
+          }}
+        >
+          Link 3
+        </li>
+      </ul>
+    </nav>
   );
 };
 
