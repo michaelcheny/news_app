2592e2f1cdbcec4488e29d0ce52808f7ef55537b
Michael Chen <michaelchengaming@gmail.com>
2020-06-04 12:07:24 -0700

    add react router dom that links to dashboard and add 404 no match

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import DashboardPage from "./containers/DashboardPage";
+import NoMatch from "./components/NoMatch";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-      <h1>Hello World</h1>
-    </div>
+      <div className="App">
+        <Router>
+          <Switch>
+            {/* <h1>Hello World</h1> */}
+            <Route path="/" exact component={DashboardPage} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </div>
+    </>
   );
 }
 
---
+++ src/containers/DashboardPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>HELLO WORLD</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
