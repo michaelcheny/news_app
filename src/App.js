d60860d7fc9ac59a43d0efbcc4494787070ab9bf
Michael Chen <michaelchengaming@gmail.com>
2020-06-04 11:19:22 -0700

    delete junk files and add navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Hello World</h1>
     </div>
   );
